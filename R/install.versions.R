2a601acfe15bc5772d1c5fe7d49a766c4b1c7245
Nick Golding <goldingn35@gmail.com>
2015-09-17 09:04:34 +0100

    adding skeleton of install.versions

---
+++ R/install.versions.R
@@ -0,0 +1,45 @@
+#' install.versions
+#'
+#' Install Specific Package Versions from the MRAN Server
+#'
+#' @description Download and install named versions of packages hosted on
+#'  CRAN from the MRAN server.
+#'
+#' @param pkgs character vector of the names of packages that should be
+#'  downloaded and installed
+#' @param versions character vector of the versions of packages to be
+#'  downloaded and installed. If this has the same length as \code{pkgs}
+#'  versions will correspond to those packages. If this has length one
+#'  the same version will be used for all packages. If it has any other
+#'  length an error will be thrown.
+#'
+#' @param lib character vector giving the library directories where to
+#'  install the packages. Recycled as needed. If missing, defaults to the
+#'  first element of \code{\link{.libPaths}()}.
+#'
+#' @param \dots other arguments to be passed to \code{\link{install.packages}}.
+#'  The arguments \code{repos} and \code{contriburl} (at least) will
+#'  be ignored as the function uses the MRAN server to retrieve package versions.
+#'
+#' @export
+#' @name install.versions
+install.versions <- function (pkgs,
+                              versions,
+                              lib,
+                              ...) {
+
+  if (!inherits(verions, 'character')) {
+    stop ('versions must be a character vector')
+  }
+
+  if (length(versions) == 1) {
+    rep(versions, length(pkgs))
+  }
+
+  if (length(versions) != length(pkgs)) {
+    stop ('versions must be have either length one, or the same length as pkgs')
+  }
+
+  install.packages(pkgs, lib, ...)
+
+}
