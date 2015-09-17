669acc289d9a451a8bb614301d11c5a9c75aa821
Nick Golding <goldingn35@gmail.com>
2015-09-17 10:44:50 +0100

    adding examples and filling in install.dates

--- R/install.dates.R
+++ R/install.dates.R
@@ -9,10 +9,12 @@
 #'  downloaded and installed
 #'
 #' @param dates character or Date vector of the dates for which to install the
-#' latest versions of \code{pkgs}. If this has the same length as \code{pkgs}
+#' latest versions of \code{pkgs}. If a data vector, it must be in the format
+#' 'yyyy-mm-dd', e.g. '2014-09-17'. If this has the same length as \code{pkgs}
 #'  versions will correspond to those packages. If this has length one
 #'  the same version will be used for all packages. If it has any other
-#'  length an error will be thrown.
+#'  length an error will be thrown. Dates before 2014-09-17 will cause an error
+#'  as MRAN does not archive before that date.
 #'
 #' @param lib character vector giving the library directories where to
 #'  install the packages. Recycled as needed. If missing, defaults to the
@@ -24,28 +26,65 @@
 #'
 #' @export
 #' @name install.dates
+#' @examples
+#'
+#' \dontrun{
+#' # install yesterday's version of checkpoint
+#' install.dates('checkpoint', Sys.Date() - 1)
+#'
+#' # install yesterday's versions of checkpoint and devtools
+#' install.dates(c('checkpoint', 'devtools'), Sys.Date() - 1)
+#'
+#' # install yesterday's version of checkpoint and the day before's devtools
+#' install.dates(c('checkpoint', 'devtools'), Sys.Date() - 1:2)
+#'
+#' }
 install.dates <- function (pkgs,
                            dates,
                            lib,
                            ...) {
+
+  # number of packages to install
+  n_pkgs <- length(pkgs)
 
   if (!inherits(dates, c('character', 'Date'))) {
     stop ('dates must be a vector of class character or Date')
   }
 
   if (length(dates) == 1) {
-    rep(dates, length(pkgs))
+    dates <- rep(dates, n_pkgs)
   }
 
-  if (length(dates) != length(pkgs)) {
+  if (length(dates) != n_pkgs) {
     stop ('dates must be have either length one, or the same length as pkgs')
   }
 
-  if (inherits(dates, 'Date')) {
-    # coerce dates to character
+  # coerce dates to character
+  dates <- as.character(dates)
 
+  # check none of the dates are before the first MRAN date
+  if (any(as.Date(dates) < as.Date('2014-09-17'))) {
+    stop (sprintf('cannot install packages before 2014-09-17 as this is the
+                  earliest date archived on MRAN.
+                  Found date: %s',
+                  min(as.Date(dates))))
   }
 
-  install.packages(pkgs, lib, ...)
+  # loop through packages installing them
+  for (i in 1:n_pkgs) {
+
+    # get package and date
+    pkg <- pkgs[i]
+    date <- dates[i]
+
+    # define repository
+    repos <- paste0('https://MRAN.revolutionanalytics.com/snapshot/', date)
+
+    install.packages(pkgs = pkg,
+                     lib = lib,
+                     repos = repos,
+                     ...)
+
+  }
 
 }
